a8588944299a7e968fac767f38d550dfd5c79973
Will Goldie <will.goldie@mail.utoronto.ca>
2023-02-12 03:16:19 +0000

    work

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,113 @@
-import React, { useCallback, useState } from 'react';
+import React, { useRef, useMemo, useCallback, useState, useEffect } from 'react';
 import "./App.css";
-import imagePages from './image_links.json'
+import imagePagesLocal from './image_links.json'
 
-const imagePagesFlat = Object.values(imagePages).flatMap(f => Object.values(f).flatMap(x => x.images))
-
-for (let i = imagePagesFlat.length -1; i > 0; i--) {
-  let j = Math.floor(Math.random() * (i + 1));
-  [imagePagesFlat[i], imagePagesFlat[j]] = [imagePagesFlat[j], imagePagesFlat[i]];
-}
-console.log(imagePages, imagePagesFlat)
+const IGNORE_REGEX = /(Runway Details)|(Backstage)/
+const MAX_COUNT = 10;
 
 function App() {
+  const [imagePages, setImagePages] = useState(imagePagesLocal);
+  useEffect(() => {
+    fetch('https://d2r56ry5w7mshh.cloudfront.net/image_links.json').then(response => {
+      if (!response.ok) { return; }
+      return response.json();
+    }).then(data => setImagePages(data))
+  }, [setImagePages])
   const ref = React.useRef();
+
+  const imagePagesFlat = useMemo(() => {
+    if(!imagePages) { return null; }
+    const flat = Object.values(imagePages).flatMap(f => Object.values(f).flatMap(x => x.collection.match(IGNORE_REGEX) ? [] : x.images))
+
+    for (let i = flat.length -1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [flat[i], flat[j]] = [flat[j], flat[i]];
+    }
+    return flat;
+
+  }, [imagePages]);
 
   const [revealed, setRevealed] = useState(false);
   const [score, setScore] = useState(0);
   const [index, setIndex] = useState(0);
+  const [count, setCount] = useState(0);
 
+  const stillPlaying  = count < MAX_COUNT;
   const handleReveal = useCallback(() => {
     setRevealed(true);
   }, [setRevealed]);
   const handleYes = useCallback(() => {
+    if (!stillPlaying) { return; }
+    if (!revealed) { return; }
     setRevealed(false);
+    setCount(count+1);
     setScore(score+1);
     setIndex(index+1);
-  }, [score, setScore, index, setIndex]);
+  }, [score, setScore, index, setIndex, stillPlaying, revealed]);
   const handleNo = useCallback(() => {
+    if (!stillPlaying) { return; }
+    if (!revealed) { return; }
+    setCount(count+1);
     setRevealed(false);
-    setScore(score-1);
     setIndex(index+1);
-  }, [score, setScore, index, setIndex]);
+  }, [score, setScore, index, setIndex, stillPlaying, revealed]);
+  const handleAgain = useCallback(() => {
+    if (stillPlaying) { return; }
+    if (!revealed) { return; }
+    setRevealed(false);
+    setIndex(index+1);
+    setCount(0);
+    setScore(0);
+  }, [score, setScore, index, setIndex, stillPlaying, revealed]);
+
+  const handler = useRef();
+
+  const handleKey = useCallback((e) => {
+    console.log(e.code)
+    if (e.code == 'Space') { handleReveal(); }
+    if (e.code == 'KeyY') { handleYes(); }
+    if (e.code == 'KeyN') { handleNo(); }
+    if (e.code == 'Enter') { handleAgain(); }
+  }, [handleReveal, handleYes, handleNo, handleAgain]);
+
+  useEffect(() => {
+    if (handler.current) {
+      window.removeEventListener('keydown', handler.current);
+    }
+    handler.current = handleKey;
+    window.addEventListener('keydown', handleKey);
+    return () => { if(handler.current) { window.removeEventListener('keydown', handler.current) } };
+  }, [handleKey]);
+
+  if (!imagePagesFlat) { 
+    return <div>Loading...</div>
+  }
 
   return (
     <div className="reference-frame">
     <div className={`top-cover ${revealed ? 'top-cover--revealed' : ''}`} >
-        <h1 className={`score ${score > 0 ? 'score--good' : ''} ${score < 0 ? 'score--bad' : ''}`}>
+    <div className="top-content">
+        <h1 className="logo">RunwayGuessr</h1>
+    {stillPlaying ? (
+        <h2>
+          {count+1}/{MAX_COUNT}
+        </h2>
+    ): <h2>Game Over</h2>}
+        <h2 className={`score ${score > (count * 2 / 3) ? 'score--good' : ''} ${score < count / 3 ? 'score--bad' : ''}`}>
           Score: {score}
-        </h1>
+        </h2>
+    </div>
     </div>
       <iframe ref={ref} height="100%" width="100%" src={imagePagesFlat[index]} frameborder="0"></iframe>
       <div className="bottom-cover">
-        <div className="controls">
-    { revealed ? <button onClick={handleYes} disabled={!revealed}  className="yes">Yes</button> : null }
-            <button onClick={handleReveal} disabled={revealed} className="reveal">Reveal</button>
-    { revealed ? <button onClick={handleNo} disabled={!revealed} className="no">No</button> : null}
-        </div>
-      </div>
+    {count < MAX_COUNT ? (
+      <div className="controls">
+        { revealed ? <button onClick={handleYes} disabled={!revealed}  className="yes">Yes<div className='hint'>(y)</div></button> : null }
+            <button onClick={handleReveal} disabled={revealed} className="reveal">Reveal<div className='hint'>(space)</div></button>
+    { revealed ? <button onClick={handleNo} disabled={!revealed} className="no">No<div className='hint'>(n)</div></button> : null}
+      </div>)
+      : (<div className='controls'><button onClick={handleAgain} className="play-again">Play Again<div className='hint'>(enter)</div></button></div>)}
+    </div>
     </div>
   );
 }
